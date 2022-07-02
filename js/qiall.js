ebaae3d91c7a59b060e6c2e4c81d414f7a7c11fb
josedoad2022 <108563897+josedoad2022@users.noreply.github.com>
2022-07-02 12:59:24 +0700

    Add files via upload

---
+++ js/qiall.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$("#divAdLeft").sticky({topSpacing:40}),$("#divAdRight").sticky({topSpacing:40})})),$(document).scroll((function(){$(window).scrollTop()>=210?$(".pos-vi").css("display","block"):$(".pos-vi").css("display","none")})),$(document).ready((function(){$(".scrollup").click((function(){return $("html, body").animate({scrollTop:0},600),!1}))}));
